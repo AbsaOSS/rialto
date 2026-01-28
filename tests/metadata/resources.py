c09f5381bfa511b46759a1b31f64ab90d1da2611
Marek Dobransky <marek.dobransky@absa.africa>
2026-01-28 13:21:05 +0100

    Revert "template decorator (#46)"
    This reverts commit 97fcf030831c14e0782d84df9b69322b6411e61c.

--- tests/metadata/resources.py
+++ tests/metadata/resources.py
@@ -31,7 +31,6 @@ feature_schema = StructType(
         StructField("feature_name", StringType(), True),
         StructField("feature_type", StringType(), True),
         StructField("feature_description", StringType(), True),
-        StructField("feature_template", StringType(), True),
         StructField("group_name", StringType(), True),
     ]
 )
@@ -42,8 +41,8 @@ group_base = [
 ]
 
 feature_base = [
-    ("Feature1", "nominal", "feature1", "template1", "Group2"),
-    ("Feature2", "nominal", "feature2", "template2", "Group2"),
+    ("Feature1", "nominal", "feature1", "Group2"),
+    ("Feature2", "nominal", "feature2", "Group2"),
 ]
 
 group_md1 = GroupMetadata(
@@ -65,6 +64,4 @@ group_md2 = GroupMetadata(
     features=["Feature1", "Feature2"],
 )
 
-feature_md1 = FeatureMetadata(
-    name="Feature1", value_type=ValueType.nominal, description="feature1", template="template1", group=group_md2
-)
+feature_md1 = FeatureMetadata(name="Feature1", value_type=ValueType.nominal, description="feature1", group=group_md2)
